c850245b3e79e816f86b460fdc7f11f687bc6b24
richa2512 <richakinjal4@email.com>
2025-11-05 11:02:24 +0530

    Added some animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,23 @@ if (toggle && menu) {
     menu.classList.toggle("hidden");
   });
 }
+
+
+const elements = document.querySelectorAll(
+  '.reveal, .fade-up, .fade-left, .fade-right, .scale-up'
+);
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      console.log("Observing:", entry);
+      if (entry.isIntersecting) {
+        console.log("Intersecting:", entry.target);
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  { threshold: 0.15 }
+);
+
+elements.forEach(el => observer.observe(el));
